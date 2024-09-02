0e984c8c8c4c70540e622fd32cda3704b9253936
Walter <w.estebancr@gmail.com>
2024-09-02 09:48:12 +0200

    search feature added

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -11,11 +11,15 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET', 'POST'])
+@app.route('/api/posts', methods=['GET'])
 def get_posts():
     if request.method == 'GET':
         return jsonify(POSTS)
 
+
+
+app.route('/api/posts', methods=['POST'])
+def add():
     if request.method == 'POST':
         data = request.get_json()
         new_id = 0
@@ -34,15 +38,53 @@ def get_posts():
         return jsonify(data)
 
 
+def delete(id):
+    for post in POSTS:
+        if int(id) in post.values():
+            if int(post["id"]) == int(id):
+                POSTS.remove(post)
+                return jsonify({"message": "Post with id {id} has been deleted successfully."})
+        else:
+            return jsonify({"error: no id found"}), 404
 
+@app.route('/api/posts/<id>', methods=['PUT'])
+def update(id):
+    if request.method == 'PUT':
+        data = request.get_json()
+        for post in POSTS:
+            if int(id) in post.values:
+                if int(id) == int(post["id"]):
+                    if "title" in data.keys():
+                        post["title"] = data["title"]
+                    else:
+                        pass
+                    if "content" in data.keys():
+                        post["content"] = data["content"]
+                    else:
+                        pass
+                return jsonify(post)
+            else:
+                return jsonify({"error: id not found"})
 
-
-
-
-
-
-
-
+@app.route('/api/posts/search')
+def search():
+    search_result = []
+    title = request.args.get("title")
+    content = request.args.get("content")
+    if title:
+        if content:
+            for post in POSTS:
+                if title in post["title"] and content in post["content"]:
+                    search_result.append(post)
+        else:
+            for post in POSTS:
+                if title in post["title"]:
+                    search_result.append(post)
+    elif content:
+        for post in POSTS:
+            if content in post["content"]:
+                search_result.append(post)
+    return jsonify(search_result)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
